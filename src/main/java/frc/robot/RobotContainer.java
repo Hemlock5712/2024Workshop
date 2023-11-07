35b047c4bb972f730fa35041d074287964171073
Team 5712 <haltom@hemlockps.com>
2023-11-07 18:44:45 -0500

    Add controller

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,13 +6,21 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.commands.SpinMotorFast;
+import frc.robot.subsystems.FlyWheel;
 
 public class RobotContainer {
+  private final CommandXboxController controller = new CommandXboxController(0);
+  private final FlyWheel flyWheel = new FlyWheel();
+
   public RobotContainer() {
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    controller.a().whileTrue(new SpinMotorFast(flyWheel));
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
