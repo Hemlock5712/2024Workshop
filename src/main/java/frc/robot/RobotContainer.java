d9946932126ec927d578338acd97e259f37c0dab
JosephTLockwood <joeojazz@gmail.com>
2023-11-09 16:40:49 -0500

    Add controller button

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,12 +7,17 @@ package frc.robot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.commands.Intake;
 import frc.robot.commands.SpinMotorFast;
+import frc.robot.subsystems.FalconIntake;
 import frc.robot.subsystems.FlyWheel;
+import frc.robot.util.TunableNumber;
 
 public class RobotContainer {
   private final CommandXboxController controller = new CommandXboxController(0);
   private final FlyWheel flyWheel = new FlyWheel();
+  private final FalconIntake intake = new FalconIntake();
+  private TunableNumber intakeSpeed = new TunableNumber("Intake Speed", 0);
 
   public RobotContainer() {
     configureBindings();
@@ -20,6 +25,7 @@ public class RobotContainer {
 
   private void configureBindings() {
     controller.a().whileTrue(new SpinMotorFast(flyWheel));
+    controller.b().whileTrue(new Intake(intake, intakeSpeed));
   }
 
   public Command getAutonomousCommand() {
