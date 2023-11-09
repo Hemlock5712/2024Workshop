3a24313d8205e7dc38635ed10ab6be96d87497e2
JosephTLockwood <joeojazz@gmail.com>
2023-11-09 17:52:50 -0500

    Run Falcon 500 and Spark Max with encoder

---
+++ src/main/java/frc/robot/commands/SpinSparkMaxEncoder.java
@@ -0,0 +1,42 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.SparkMaxEncoder;
+import frc.robot.util.TunableNumber;
+
+public class SpinSparkMaxEncoder extends Command {
+  SparkMaxEncoder m_sparkMaxEncoder;
+  double m_targetSpeed = 0;
+  /** Creates a new SpinSparkMaxEncoder. */
+  public SpinSparkMaxEncoder(SparkMaxEncoder sparkMaxEncoder, TunableNumber targetSpeed) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    m_sparkMaxEncoder = sparkMaxEncoder;
+    m_targetSpeed = targetSpeed.get();
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_sparkMaxEncoder.setFlyWheelSpeed(m_targetSpeed);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_sparkMaxEncoder.setFlyWheelSpeed(0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/SparkMaxEncoder.java
@@ -0,0 +1,47 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class SparkMaxEncoder extends SubsystemBase {
+  private CANSparkMax motor = new CANSparkMax(8, MotorType.kBrushless);
+  private CANcoder encoder = new CANcoder(1);
+  private double kP = 0.0;
+  private double kI = 0.0;
+  private double kD = 0.0;
+  private PIDController pidController = new PIDController(kP, kI, kD);
+  private double targetSpeed = 0;
+  
+  /** Creates a new SparkMaxEncoder. */
+
+  public SparkMaxEncoder() {
+    motor.setIdleMode(CANSparkMax.IdleMode.kBrake);
+  }
+
+  public void setFlyWheelSpeed(double speed) {
+    targetSpeed = speed;
+  }
+
+  public double getFlyWheelSpeed(){
+    return targetSpeed;
+  }
+
+  public double getEncoderValue() {
+    return encoder.getVelocity().getValueAsDouble();
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    double error = pidController.calculate(getEncoderValue(), targetSpeed);
+    motor.set(error);
+  }
+}
